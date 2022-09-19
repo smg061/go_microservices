171f9147951a0442a3f745debe03efee19670a95
Santos Gonzalez <max.go.95@gmail.com>
2022-09-18 23:47:10 -0500

    made store, started working on index

---
+++ log/internal/log/store.go
@@ -0,0 +1,98 @@
+package log 
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	mu *sync.Mutex
+	buf *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+
+	return &store{
+		File: f,
+		size: size,
+		buf: bufio.NewWriter(f),
+		mu: &sync.Mutex{},
+	}, nil
+}
+
+func (s *store) Append (p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0,0, err
+	}
+	w+= lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+func (s *store) Read (pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// every time you flush, the writer dumps to the file
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	// read the size from the log at the position into the size byte slice (size of the log)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	// make a buffer of the size of obtained size (this is the size of the log)
+	b := make([]byte, enc.Uint64(size))
+	// read the actual contents of the file
+	// pos + lendwith is where the log starts
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
+}
